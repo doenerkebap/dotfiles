b79a92ba9d4f092100791006a0fa126c70a84e96
doener <doenerkebap@mailbox.org>
2018-09-05 23:11:06 +0200

    xmonad/zsh: start new terminal in last directory (dirty)

--- xmonad/xmonad.config/xmonad.hs
+++ xmonad/xmonad.config/xmonad.hs
@@ -9,7 +9,7 @@ main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ desktopConfig
-        { terminal = "termite"
+        { terminal = "termite -d $(cat $XDG_CACHE_HOME/last_dir)"
         , manageHook = manageDocks <+> manageHook desktopConfig
         , layoutHook = avoidStruts  $  layoutHook desktopConfig
         , logHook = dynamicLogWithPP xmobarPP
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,3 +2,4 @@ autoload -U promptinit; promptinit
 prompt spaceship
 
 SPACESHIP_VI_MODE_SHOW=false
+alias spaceship_prompt='pwd > $XDG_CACHE_HOME/last_dir; spaceship_prompt'
