d51a997928731dec254b55df3f0cf69a89cfb6be
doener <doenerkebap@mailbox.org>
2018-09-08 23:15:01 +0200

    zsh: use base16 and configure prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,13 @@
 autoload -U promptinit; promptinit
 prompt spaceship
 
-SPACESHIP_VI_MODE_SHOW=false
+SPACESHIP_GIT_PREFIX="%F{20}on "
+SPACESHIP_GIT_STATUS_COLOR=16
+SPACESHIP_EXEC_TIME_PREFIX="%F{20}took "
+SPACESHIP_EXIT_CODE_SHOW=true
+SPACESHIP_VI_MODE_COLOR=20
+SPACESHIP_VI_MODE_SUFFIX=""
+SPACESHIP_VI_MODE_INSERT=" I "
+SPACESHIP_VI_MODE_NORMAL=" N "
 alias spaceship_prompt='pwd > $XDG_CACHE_HOME/last_dir; spaceship_prompt'
+spaceship_vi_mode_enable
