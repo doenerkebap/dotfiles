446addc9bcc0514d7afeacbbc7899a246245af61
doener <doenerkebap@mailbox.org>
2018-09-15 09:09:04 +0200

    xmonad: use solarized colors in xmobar. (WIP)

--- xmonad/xmonad.config/xmonad.hs
+++ xmonad/xmonad.config/xmonad.hs
@@ -3,17 +3,12 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog (xmobar)
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "xmobar"
-
-    xmonad $ desktopConfig
+    xmonad =<< xmobar desktopConfig
         { terminal = "termite -d $(cat $XDG_CACHE_HOME/last_dir)"
         , manageHook = manageDocks <+> manageHook desktopConfig
         , layoutHook = avoidStruts  $  layoutHook desktopConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                { ppOutput = hPutStrLn xmproc
-                , ppTitle = xmobarColor "green" "" . shorten 50
-                }
         }
