0cb42e1b442dcc63e27293719d87e968e0979493
doener <doenerkebap@mailbox.org>
2019-01-29 21:54:28 +0100

    xmonad: bind audio and brightness keys and add resizable tall layout.

--- xmonad/xmonad.config/xmonad.hs
+++ xmonad/xmonad.config/xmonad.hs
@@ -1,14 +1,32 @@
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
+import XMonad.Layout.TwoPane
+import XMonad.Layout.ResizableTile
+import qualified Data.Map as M
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog (xmobar)
-import System.IO
+import Graphics.X11.ExtraTypes.XF86
+-- import XMonad.Hooks.DynamicLog
+-- import XMonad.Util.Run(spawnPipe)
+-- import System.IO
 
 main = do
     xmonad =<< xmobar desktopConfig
-        { terminal = "termite -d $(cat $XDG_CACHE_HOME/last_dir)"
+        { terminal = myTerminal
+	, keys = myKeys <+> keys desktopConfig
         , manageHook = manageDocks <+> manageHook desktopConfig
-        , layoutHook = avoidStruts  $  layoutHook desktopConfig
+	, layoutHook = avoidStruts $ myLayoutHook
         }
+
+myTerminal = "termite -d $(cat $XDG_CACHE_HOME/last_dir)"
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+    [ ((modm, xK_u), sendMessage MirrorShrink)
+    , ((modm, xK_i), sendMessage MirrorExpand)
+    , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
+    , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+    , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 4")
+    , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 4")
+    ]
+myLayoutHook = Full ||| ResizableTall 1 (2/100) (1/2) [] ||| TwoPane (2/100) (1/2) ||| Mirror (Tall 1 (2/100) (1/2))
+
