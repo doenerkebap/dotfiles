df58af90f5826d0b5dadd9f3ce68beb0a33fa903
doener <doenerkebap@mailbox.org>
2019-02-02 14:42:58 +0100

    dmenu, xmonad: Lauch solarized dmenu with Mod+P.

--- xmonad/xmonad.config/xmonad.hs
+++ xmonad/xmonad.config/xmonad.hs
@@ -22,6 +22,7 @@ myTerminal = "termite -d $(cat $XDG_CACHE_HOME/last_dir)"
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
     [ ((modm, xK_u), sendMessage MirrorShrink)
     , ((modm, xK_i), sendMessage MirrorExpand)
+    , ((modm, xK_p), spawn "$ZSH/dmenu/dmenu_run-dark")
     , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
     , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
     , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
